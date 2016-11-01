a5f9f3a5962708b7c55601d1fc53a4aa4ece925e
destintrang <destintrang@gmail.com>
2016-10-31 18:23:45 -0700

    scene control

---
+++ Project B.L.U.D/Assets/Scripts/ObjectLoadPress.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectLoadPress : MonoBehaviour {
+	public GameObject objneedshow;
+
+	void OnMouseDown(){
+		Debug.Log ("clicked");
+	}
+}
