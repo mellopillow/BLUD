b95481d283f031751c87d440aeb7b2498fe7eb52
Daniel Daugherty <ddaugher@uci.edu>
2016-10-31 17:51:39 -0700

    movement stuff

--- Project B.L.U.D/Assets/PlayerMovement.cs
+++ Project B.L.U.D/Assets/PlayerMovement.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Diagnostics;
 
 public class PlayerMovement : MonoBehaviour {
 
     // Use this for initialization
     public float speed = 4;
-    //public float gravity = -3;
+    public bool jumping = false;
+    public float gravity = -1;
     Vector3 velocity;
     Rigidbody2D playerRB;
+    Stopwatch s = new Stopwatch();
 
 	void Start () {
         playerRB = GetComponent<Rigidbody2D>();
@@ -20,9 +23,32 @@ public class PlayerMovement : MonoBehaviour {
         Move(h, v);
 	}
     
-    void Move(float h, float v) {
+    void Move(float h, float v){
         velocity.x = h * speed/50;
-        //velocity.y = gravity * Time.deltaTime;
+        velocity.y = gravity * Time.deltaTime;
         transform.Translate(velocity);
+    }
+
+    void Jump(float v){
+        if (v>0 && jumping == false)
+        {
+            s.Start();
+            jumping = true;
+            gravity = 0;
+            velocity.y = v * speed/3;
+            transform.Translate(velocity);
+        }
+        if(s.ElapsedMilliseconds > 200)
+        {
+            velocity.y = 0;
+            gravity = -1;
+        }
+        if (s.ElapsedMilliseconds > 600)
+        {
+            s.Reset();
+            jumping = false;
+        }
+
+
     }
 }
