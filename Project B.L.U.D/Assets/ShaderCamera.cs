a7fd1e6c368f9294c8af0afe19549537d5ff40c7
Emil Mombay <emombay@uci.edu>
2016-11-07 18:11:23 -0800

    Added Spotlight functionality
    Spotlight shader moves with your mouse to only light up where the mouse
    is.

---
+++ Project B.L.U.D/Assets/ShaderCamera.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderCamera : MonoBehaviour
+{
+    public Material Mat;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    
+
+    public void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        //Shader.Find(Spotlight);
+        Debug.Log(Mat.shader);
+        Debug.Log("Hello?");
+
+        float xPos = mousePos.x / Screen.width * 0.5f;
+        float yPos = (Screen.height - mousePos.y) / Screen.height * (0.5f * 9)/16;
+        Mat.SetFloat(Shader.PropertyToID("_CenterX"), xPos);
+        Mat.SetFloat(Shader.PropertyToID("_CenterY"), yPos);
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, Mat);
+    }
+}
