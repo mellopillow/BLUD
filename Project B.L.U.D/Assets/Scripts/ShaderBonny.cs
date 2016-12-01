f3d5f19a231eebc1efcbbc1ba74f2a54e798a6d3
Emil Mombay <emombay@uci.edu>
2016-11-30 16:24:00 -0800

    spotlight changes

---
+++ Project B.L.U.D/Assets/Scripts/ShaderBonny.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShaderBonny : MonoBehaviour
+{
+    public Material Mat;
+
+    void Start()
+    {
+        //Cursor.visible = false;
+    }
+
+
+    public void Update()
+    {
+        Vector3 bonnyPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        //Shader.Find(Spotlight);
+        Debug.Log(Mat.shader);
+        Debug.Log("Hello?");
+
+        float xPos = bonnyPos.x / Screen.width * 0.5f;
+        float yPos = bonnyPos.y / Screen.height * (0.5f * 9) / 16;
+        Mat.SetFloat(Shader.PropertyToID("_CenterX"), xPos);
+        Mat.SetFloat(Shader.PropertyToID("_CenterY"), yPos);
+    }
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, Mat);
+    }
+}
