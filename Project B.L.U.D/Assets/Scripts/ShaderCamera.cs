804327b7fab88f314772246264c9c3374155d88a
Emil Mombay <emombay@uci.edu>
2017-04-20 20:38:08 -0700

    Bonny Spotlight fix
    Spotlight is now centered on bonny

--- Project B.L.U.D/Assets/Scripts/ShaderCamera.cs
+++ Project B.L.U.D/Assets/Scripts/ShaderCamera.cs
@@ -4,9 +4,12 @@
 public class ShaderCamera : MonoBehaviour
 {
     public Material Mat;
-
+    private Transform playerTransform;
+    //GameObject player;
     void Start()
     {
+        //player = GameObject.FindGameObjectWithTag("player");
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         //Cursor.visible = false;
     }
     
@@ -14,10 +17,13 @@ public class ShaderCamera : MonoBehaviour
     public void Update()
     {
         Vector3 mousePos = Input.mousePosition;
+        Debug.Log(mousePos);
+        Vector3 playerPos = playerTransform.position;
+        Vector3 playerPosScreen = Camera.main.WorldToScreenPoint(playerPos);
         //Shader.Find(Spotlight);
 
-        float xPos = mousePos.x / Screen.width * 0.5f;
-        float yPos = (Screen.height - mousePos.y) / Screen.height * (0.5f * 9)/16;
+        float xPos = playerPosScreen.x / Screen.width * 0.5f;
+        float yPos = (Screen.height - playerPosScreen.y) / Screen.height * (0.5f * 9)/16;
         Mat.SetFloat(Shader.PropertyToID("_CenterX"), xPos);
         Mat.SetFloat(Shader.PropertyToID("_CenterY"), yPos);
     }
