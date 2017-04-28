4b409fa6226512ba7ef5f25bdae6209b91327f7c
Daniel Daugherty <ddaugher@uci.edu>
2017-04-27 19:51:25 -0700

    Loading Text + New Font!

---
+++ Project B.L.U.D/Assets/LoadText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadText : MonoBehaviour {
+
+    bool loading;
+    float timer = 0;
+    int current = 0;
+    public float loadingTime;
+    public Text GameText;
+    string t="";
+
+    public void load(string text)
+    {
+        t = text;
+        loading = true;
+    }
+
+    void Start()
+    {
+        GameText.text = "";
+    }
+
+    void Update()
+    {
+        if (loading & current<=t.Length)
+        {
+            timer += Time.deltaTime;
+            if (timer > (loadingTime/t.Length))
+            {
+                GameText.text = t.Substring(0, current);
+                current++;
+                timer = 0f;
+            }
+        }
+        else
+            loading = false;
+    }
+}
