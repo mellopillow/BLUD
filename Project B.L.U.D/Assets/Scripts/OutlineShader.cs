0e83b33313af4d900ac0facf01afa72a89285452
Emil Mombay <emombay@uci.edu>
2017-05-18 19:21:44 -0700

    Added Outline Shader
    Shader script for outlines and its material

---
+++ Project B.L.U.D/Assets/Scripts/OutlineShader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineShader : MonoBehaviour {
+
+    public Material mat;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    } 
+}
