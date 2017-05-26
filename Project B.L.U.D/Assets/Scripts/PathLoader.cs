43dec1ffdf8866f026cb7f32fd8e8b6a11427f9d
Daniel Daugherty <ddaugher@uci.edu>
2017-05-25 19:54:58 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Project B.L.U.D/Assets/Materials/bonnySpotlight.mat
    
    Added Examine Text to Text System

--- Project B.L.U.D/Assets/Scripts/PathLoader.cs
+++ Project B.L.U.D/Assets/Scripts/PathLoader.cs
@@ -13,16 +13,22 @@ public class PathLoader : MonoBehaviour
     public float ActivationProximity;
     public Sprite BaseImage;
     public Sprite ActivatedImage;
+    Text ExamineText;
     Text text;
     LoadText load;
     public string[] ot;
     //bool clicked;
     public bool isBattery = false;
+    float original_alpha;
+    bool JustExited;
+    bool JustEntered;
 
     void Start()
     {
+        original_alpha = 128;
         text = GameObject.FindWithTag("text").GetComponent<Text>();
         load = GameObject.FindWithTag("text").GetComponent<LoadText>();
+        ExamineText = GameObject.FindWithTag("inspect").GetComponent<Text>();
     }
 
     void Update(){
@@ -47,13 +53,36 @@ public class PathLoader : MonoBehaviour
 
 		if (CheckCloseTo("Player", ActivationProximity))
 		{
+            JustExited = false;
 			text.material.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a /*- Mathf.Abs((this.transform.position.x - GameObject.FindWithTag("Player").transform.position.x * text.color.a / ActivationProximity))*/);
             // fades text in and out based on distance between player and object
-            gameObject.GetComponent<SpriteRenderer>().sprite = ActivatedImage; 
-		}
+            gameObject.GetComponent<SpriteRenderer>().sprite = ActivatedImage;
+            if (!load.isLoading())
+            {
+                ExamineText.text = "examine";
+                print(text.color.a - Mathf.Abs((transform.position.x -
+                        GameObject.FindWithTag("Player").transform.position.x * text.color.a / ActivationProximity)));
+                    ExamineText.material.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a - Mathf.Abs((transform.position.x -
+                        GameObject.FindWithTag("Player").transform.position.x * text.color.a / ActivationProximity)));
+                RectTransform CanvasRect = GameObject.FindWithTag("canvas").GetComponent<RectTransform>();
+                Vector2 ViewportPosition = GameObject.FindWithTag("MainCamera").GetComponent<Camera>().WorldToViewportPoint(transform.position);
+                Vector2 WorldObject_ScreenPosition = new Vector2(
+                ((ViewportPosition.x * CanvasRect.sizeDelta.x) - (CanvasRect.sizeDelta.x * 0.5f))+ CanvasRect.sizeDelta.x / 20,
+                ((ViewportPosition.y * CanvasRect.sizeDelta.y) - (CanvasRect.sizeDelta.y * 0.5f))+ViewportPosition.y*CanvasRect.sizeDelta.y/2);
+
+                //now you can set the position of the ui element
+                ExamineText.GetComponent<RectTransform>().anchoredPosition = new Vector2(WorldObject_ScreenPosition.x, WorldObject_ScreenPosition.y);
+            }
+        }
 
 		else {
-			//clicked = false;
+            ExamineText.material.color = new Color(text.color.r, text.color.g, text.color.b, text.color.a);
+            //clicked = false;
+            if (!JustExited)
+            {
+                ExamineText.text = "";
+                JustExited = true;
+            }
 			gameObject.GetComponent<SpriteRenderer>().sprite = BaseImage;
             //load.load("");
 		}
