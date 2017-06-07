339b516bd4fd119c7d18ee75bed113e0d0fb2b41
Emil Mombay <emombay@uci.edu>
2017-06-06 20:07:16 -0700

    scene changes
    update the canvas

--- Project B.L.U.D/Assets/Scripts/Key.cs
+++ Project B.L.U.D/Assets/Scripts/Key.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Key : MonoBehaviour {
 
 	public float proximity;
+    public GameObject key;
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +15,10 @@ public class Key : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown ("space")){
+		if (Input.GetKeyDown ("e")){
 			if (CheckCloseTo ("Player", proximity)) {
 				ItemScript.key = true;
+                key.SetActive(true);
 			}		
 		}
 	}
